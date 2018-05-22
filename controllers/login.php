89c8f84cc119ffd6e7270021dc16b2cef6c83f25
Marche <marche_871@hotmail.com>
2018-05-22 14:20:36 -0300

    permisos

--- controllers/login.php
+++ controllers/login.php
@@ -8,37 +8,8 @@
 		
 		function init(){
 		include('templates/login.html');	
-		}
-			
-        function ingresar(){
-            include('php_old/connection.php');
-            $con = connect();
-			$usuario = isset($_POST["usuario"]) ? $_POST["usuario"] : die;
-            $pass = isset($_POST["contraseña"]) ? $_POST["contraseña"] : die;
-		/*	echo "<h1 class='white'>Usuario: ".$usuario."</h1>";
-			echo "<h1 class='white'>Pass: ".$pass."</h1>";*/
-			
-		
-            
-        //    $usuario = $_POST["usuario"];
-        //    $pass = $_POST["contraseña"];
-            $sql = "SELECT * FROM usuarios WHERE nombre='$usuario' and pass='$pass'";
-			$resultado = mysqli_query($con, $sql) or die (mysqli_error($con));
-            $vUsuario = mysqli_fetch_assoc($resultado);
-            mysqli_close($con);
-            if(!is_null($vUsuario)){
-                $_SESSION["usuario"] = $vUsuario["nombre"];
-                //setPermisos($vUsuario["tipo"]);
-                echo '<pre>' . print_r($_SESSION, TRUE) . '</pre>';
-                header("Location:home.php");
-             //   $loged = True;            
-            }
-            else{
-               // $loged = false;
-                echo("Usuario o contrasena incorrecta");
-            }
         }
-
+        
         function setPermisos($tipoUsuario){
             echo("tipo: ".$tipoUsuario);
             $query = "SELECT p.titulo as 'titulo'
@@ -51,19 +22,35 @@
             $c = connect();
             $result = mysqli_query($c, $query) or die (mysqli_error($c));
             $permisos = mysqli_fetch_array($result);
+            //echo '<pre>' . print_r($permisos, TRUE) . '</pre>';
             mysqli_close($c);
-            $i = 0;
-            while($p = mysqli_fetch_assoc($result)){
-                $_SESSION["permisos"][$i] = $p;
-                //print_r($p[$i]);
-                $i++;
-            }
-			
-            //$_SESSION["permisos"] = $permisos;
-            // foreach($permisos as $per){
-            //     echo $per;
-            // }
+            $_SESSION['permisos'] = [];
+            $_SESSION['permisos'] = $permisos[0];
+            $_SESSION['permisos'] = $permisos[1];
         }
+			
+        function ingresar(){
+            include('connection.php');
+            $con = connect();
+			$usuario = isset($_POST["usuario"]) ? $_POST["usuario"] : die;
+            $pass = isset($_POST["contraseña"]) ? $_POST["contraseña"] : die;
+            $sql = "SELECT * FROM usuarios WHERE nombre='$usuario' and pass='$pass'";
+			$resultado = mysqli_query($con, $sql) or die (mysqli_error($con));
+            $vUsuario = mysqli_fetch_assoc($resultado);
+            mysqli_close($con);
+            if(!is_null($vUsuario)){
+                $_SESSION["usuario"] = $vUsuario["nombre"];
+                $this->setPermisos($vUsuario["tipo"]);
+                header("Location:home.php");
+             //   $loged = True;            
+            }
+            else{
+               // $loged = false;
+                echo("Usuario o contrasena incorrecta");
+            }
+        }
+
+        
 		// include_once('../templates/footer.php');
 		}
 	?>
