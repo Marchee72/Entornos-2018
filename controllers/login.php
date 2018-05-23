d82e8b424089b2170be2faa5375025581016b8bc
Franco Ferrero <francof_nob@hotmail.com>
2018-05-23 11:39:55 -0300

    login

--- controllers/login.php
+++ controllers/login.php
@@ -36,14 +36,14 @@
             $con = connect();
 			$usuario = isset($_POST["usuario"]) ? $_POST["usuario"] : die;
             $pass = isset($_POST["contraseña"]) ? $_POST["contraseña"] : die;
-            $sql = "SELECT * FROM usuarios WHERE nombre='$usuario' and pass='$pass'";
+            $sql = "SELECT * FROM usuarios WHERE usuario='$usuario' and pass='$pass'";
 
 			$resultado = mysqli_query($con, $sql) or die (mysqli_error($con));
             $vUsuario = mysqli_fetch_assoc($resultado);
             //sqli_free_result($resultado);
             mysqli_close($con);
             if(!is_null($vUsuario)){
-                $_SESSION["usuario"] = $vUsuario["nombre"];
+                $_SESSION["usuario"] = $vUsuario["usuario"];
                 $this->setPermisos($vUsuario["tipo"]);
 
                 header("Location:home.php");            
