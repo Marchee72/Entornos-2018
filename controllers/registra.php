13a3a2d0e4eb47955526d30c6c996fc6d7331e1b
Marche <marche_871@hotmail.com>
2018-05-23 11:12:56 -0300

    registrar usuario

--- controllers/registra.php
+++ controllers/registra.php
@@ -1,10 +1,9 @@
 <?php
- //$action = $_GET["action"];
  
  class registra{
- //include('../templates/header.php');
- //if(isset($action)){
-//	 if($action == "home") {
+	function init(){
+		include('templates/registra.html');	
+        }
 	
 	function cerveceria(){
 	echo "<h1 class='white'>Formulario registro cerveceria aca</h1>";
@@ -12,16 +11,41 @@
 	}
 	
 	function usuario(){
-	echo "<h1 class='white'>Formulario registro usuario aca</h1>";
+	include('templates/registra.html');	
+	$this->registrarNuevoUsuario();
 	}
 	
-	//	 }else{
-//	 include_once($action.'.php');	 
-//	 }
-//}else{
-//	header("Location:home.php?action=home");
-//}
-// include('../templates/footer.php');
+	function registrarNuevoUsuario(){
+		include('connection.php');
+		$con = connect();
+		$nombre =  isset($_POST["nombre"]) ? $_POST["nombre"] : die;
+		$apellido =  isset($_POST["apellido"]) ? $_POST["apellido"] : die;
+		$usuario = isset($_POST["usuario"]) ? $_POST["usuario"] : die;
+		$email =  isset($_POST["email"]) ? $_POST["email"] : die;
+		$pass = isset($_POST["contraseña"]) ? $_POST["contraseña"] : die;
+		$tipo = isset($_POST["tipo"]) ? $_POST["tipo"] : 2; //USUARIO TIPO 2 ES UN USUARIO COMUN
+		$valido = 0; //NO ES VALIDO HASTA QUE SE VERIFIQUE POR MAIL
+
+		$sql = "SELECT Count(nombre) as cantidad FROM usuarios WHERE nombre='$usuario'";
+		$resultado = mysqli_query($con, $sql) or die (mysqli_error($con));
+		$vCantUsuarios = mysqli_fetch_assoc($resultado);
+
+		if ($vCantUsuarios["cantidad"] !=0){
+			echo ("El Usuario ya Existe<br>");
+			echo ("<A href='home.html'>VOLVER AL MENU</A>");
+		}
+		else {
+			$sql = "INSERT INTO usuarios (nombre, pass, tipo, valido, nombre, apellido, email) 
+					values ('$usuario','$pass', '$tipo', '$valido', '$nombre', '$apellido', '$email')";
+			mysqli_query($con, $sql) or die (mysqli_error($con));
+			echo("El Usuario fue Registrado<br>");
+			echo ("<A href='home.html'>VOLVER AL MENU</A>");
+			// Liberar conjunto de resultados
+			mysqli_free_result($resultado);
+		}
+		// Cerrar la conexion
+		mysqli_close($con);
+	}
 
  }
 ?>
