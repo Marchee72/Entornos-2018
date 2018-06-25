92aa13ee5117da1ad664f0a67662451073c02887
axel012 <axel_012@hotmail.com.ar>
2018-06-25 15:29:24 -0300

    hay q cambiar el nombre de las db

--- Cerventum/config/config.php
+++ Cerventum/config/config.php
@@ -3,7 +3,7 @@
   
   // Server configurations
   const SERVER_NAME = "localhost";
-  const DB_USERNAME = "id6126726_axel012";//"root";
-  const DB_PASSWORD = "123456789as";//"1234";
-  const DB_NAME = "id6126726_cervesium";//"cervesium";
+  const DB_USERNAME = "root";//"id6126726_axel012";//"root";
+  const DB_PASSWORD = "1234"//123456789as";//"1234";
+  const DB_NAME = "cervesium"//"id6126726_cervesium";//"cervesium";
 ?>
